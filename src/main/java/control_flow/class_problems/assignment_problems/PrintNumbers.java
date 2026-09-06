0ed9876fd4de6a4359be22d32bca079523867894
Shruti Goswami <goswamishruti78@gmail.com>
2026-09-06 10:26:31 +0530

    Added PrintNumbers program

---
+++ src/main/java/control_flow/class_problems/assignment_problems/PrintNumbers.java
@@ -0,0 +1,13 @@
+public class PrintNumbers {
+  
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}  
+
